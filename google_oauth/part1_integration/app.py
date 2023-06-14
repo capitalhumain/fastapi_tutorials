148ab3d94f7b75e97efc4a4b6c5e469637b0bc97
Daniel VALIDE <capitalhumain@gmail.com>
2023-06-14 04:10:08 +0000

    init

--- google_oauth/part1_integration/app.py
+++ google_oauth/part1_integration/app.py
@@ -9,7 +9,8 @@ from starlette.middleware.sessions import SessionMiddleware
 from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
 
 from authlib.integrations.starlette_client import OAuth
-
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
 # Initialize FastAPI
 app = FastAPI(docs_url=None, redoc_url=None)
@@ -48,10 +49,20 @@ oauth.register(
     name='google',
     server_metadata_url=CONF_URL,
     client_kwargs={
-        'scope': 'openid email profile'
+        'scope': 'openid email profile https://www.googleapis.com/auth/webmasters.readonly'
     }
 )
-
+"""oauth.register(
+    'google',
+    client_id='1075021919607-ef54410eb57bbk89a04ji41ebsvvhqb5.apps.googleusercontent.com',
+    client_secret='9cYgtBjIsiXTWdIkO5aaz9d0',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile https://www.googleapis.com/auth/webmasters.readonly'
+    }
+)"""
 
 @app.get('/login', tags=['authentication'])  # Tag it as "authentication" for our docs
 async def login(request: Request):
@@ -68,6 +79,7 @@ async def auth(request: Request):
     user = await oauth.google.parse_id_token(request, token)
 
     # Save the user
+   
     request.session['user'] = dict(user)
 
     return RedirectResponse(url='/')
@@ -109,6 +121,49 @@ async def get_documentation(request: Request, user: Optional[dict] = Depends(get
     response = get_swagger_ui_html(openapi_url='/openapi.json', title='Documentation')
     return response
 
+
+@app.get('/protected')
+async def protected(user: Optional[dict] = Depends(get_user)):
+    # print(user)
+    if user is None:
+        raise HTTPException(status_code=403, detail='Could not validate credentials.')
+        
+    creds = Credentials(
+        token=user['token']['access_token'],
+        refresh_token=user['token']['refresh_token'],
+        token_uri=CONF_URL,
+        client_id=oauth.google.client_id,
+        client_secret=oauth.google.client_secret
+    )
+    
+    searchconsole = build('searchconsole', 'v1', credentials=creds)
+    request = {
+        "startDate": '2023-01-01',
+        "endDate": '2023-01-31',
+        "dimensions": ["searchAppearance"],
+        "rowLimit": 10,
+    }
+    results = execute_request(searchconsole, 'https://www.inpixio.com', request)
+    """results = searchconsole.searchanalytics().query().get(
+        siteUrl='https://example.com',
+        startDate='2020-01-01',
+        endDate='2020-01-31'
+    ).execute()"""
+    
+    return results 
+
+def execute_request(service, property_uri, request):
+    """Executes a searchAnalytics.query request.
+
+    Args:
+      service: The searchconsole service to use when executing the query.
+      property_uri: The site or app URI to request data for.
+      request: The request to be executed.
+
+    Returns:
+      An array of response rows.
+    """
+    return service.searchanalytics().query(siteUrl=property_uri, body=request).execute()
 
 if __name__ == '__main__':
     import uvicorn
